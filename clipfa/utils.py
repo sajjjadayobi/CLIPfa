5316196c2dbd85922cc130e98752403c9690bcb8
Sajjad Ayoubi <43270899+sajjjadayobi@users.noreply.github.com>
2021-12-13 23:14:19 +0330

    add clip_wraper_creator

--- clipfa/utils.py
+++ clipfa/utils.py
@@ -1,7 +1,9 @@
 import torch
 import matplotlib.pyplot as plt
+from torch import nn
 import gc
 import multiprocessing
+from transformers import CLIPConfig, CLIPModel
 from tqdm import tqdm
 
 from config import MEAN, STD, TOKENIZER
@@ -32,23 +34,23 @@ def show_data(batch, idx=0):
     print('Text: ', text)
 
 
-def get_image_embeddings(clip, datalodear, device='cuda'):
-    clip.eval().to(device)
+def get_image_embeddings(image_encoder, datalodear, device='cuda'):
+    image_encoder.eval().to(device)
     embeddings = []
     with torch.no_grad():
         for item in tqdm(datalodear):
-            image_embedding = clip.get_image_features(
-                pixel_values=item['pixel_values'].to(device))
+            image_embedding = image_encoder.forward(
+                pixel_values=item['pixel_values'].to(device)).pooler_output
             embeddings.append(image_embedding)
     return torch.cat(embeddings)
 
 
-def get_text_embedding(clip, query='football', device='cuda'):
-    clip.eval().to(device)
+def get_text_embedding(text_encoder, query='football', device='cuda'):
+    text_encoder.eval().to(device)
     tokens = TOKENIZER(query, return_tensors='pt')
     with torch.no_grad():
-        text_embedding = clip.get_text_features(input_ids=tokens["input_ids"].to(device),
-                                                attention_mask=tokens["attention_mask"].to(device))
+        text_embedding = text_encoder(input_ids=tokens["input_ids"].to(device),
+                                      attention_mask=tokens["attention_mask"].to(device)).pooler_output
     return text_embedding
 
 
@@ -56,3 +58,20 @@ def most_similar_images(image_embeddings, text_embeddings, images):
     values, indices = torch.cosine_similarity(
         image_embeddings, text_embeddings).sort(descending=True)
     return images[indices.cpu()]
+
+
+def clip_wraper_creator():
+    """create a dummy CLIPModel to wrap text and vision encoders in order to use CLIPTrainer"""
+    config = {'num_hidden_layers': 0,
+              'max_position_embeddings': 0,
+              'vocab_size': 0,
+              'hidden_size': 1,
+              'patch_size': 1,
+              }
+    DUMMY_CONFIG = CLIPConfig(text_config_dict=config,
+                              vision_config_dict=config)
+    clip = CLIPModel(config=DUMMY_CONFIG)
+    # convert projectors to Identity
+    clip.text_projection = nn.Identity()
+    clip.visual_projection = nn.Identity()
+    return clip
