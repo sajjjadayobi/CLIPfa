d72f54300ac67ef72f652f7376271883d2cd6fd7
Sajjad Ayoubi <43270899+sajjjadayobi@users.noreply.github.com>
2021-12-17 15:54:43 +0330

    Create config.py

---
+++ clipfa/config.py
@@ -0,0 +1,33 @@
+import torch
+
+DATA_FILE = 'data.csv'
+TEST_SIZE = 0.05
+TEXT_MODEL = 'm3hrdadfi/roberta-zwnj-wnli-mean-tokens'
+IMAGE_MODEL = 'openai/clip-vit-base-patch32'
+BATCH_SIZE = 128
+IMAGE_SIZE = 224
+MAX_LEN = 120  # decide base on the whole dataset
+MEAN = torch.tensor([0.48145466, 0.4578275, 0.40821073])
+STD = torch.tensor([0.26862954, 0.26130258, 0.27577711])
+
+if __name__ == '__main__':
+    from transformers import TrainingArguments, AutoTokenizer, CLIPFeatureExtractor
+    vision_preprocessor = CLIPFeatureExtractor.from_pretrained(IMAGE_MODEL)
+    tokenizer = AutoTokenizer.from_pretrained(TEXT_MODEL)
+    args = TrainingArguments(
+        "clip-fa",
+        evaluation_strategy="steps",
+        save_strategy="steps",
+        eval_steps=500,
+        logging_steps=50,
+        learning_rate=3e-5,
+        weight_decay=0.003,
+        warmup_steps=500,
+        fp16=False,
+        prediction_loss_only=True,
+        gradient_accumulation_steps=1,
+        per_device_train_batch_size=BATCH_SIZE,
+        per_device_eval_batch_size=BATCH_SIZE,
+        num_train_epochs=10,
+        report_to='tensorboard'
+    )
