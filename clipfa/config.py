2a47e486ae4bde5b5fb531d14e11d6b2e9d4ec90
Sajjad Ayoubi <43270899+sajjjadayobi@users.noreply.github.com>
2021-12-22 15:19:47 +0330

    Update config.py

--- clipfa/config.py
+++ clipfa/config.py
@@ -6,7 +6,7 @@ TEXT_MODEL = 'm3hrdadfi/roberta-zwnj-wnli-mean-tokens'
 IMAGE_MODEL = 'openai/clip-vit-base-patch32'
 BATCH_SIZE = 128
 IMAGE_SIZE = 224
-MAX_LEN = 100  # decide base on the whole dataset
+MAX_LEN = 64  
 MEAN = torch.tensor([0.48145466, 0.4578275, 0.40821073])
 STD = torch.tensor([0.26862954, 0.26130258, 0.27577711])
 
@@ -17,17 +17,23 @@ if __name__ == '__main__':
     args = TrainingArguments(
         "clip-fa",
         evaluation_strategy="steps",
-        save_strategy="steps",
         eval_steps=500,
-        logging_steps=50,
+        logging_steps=500,
         learning_rate=3e-5,
+        prediction_loss_only=True,
         weight_decay=0.003,
         warmup_steps=500,
-        fp16=False,
-        prediction_loss_only=True,
+        fp16=True,
+        save_strategy='steps',
+        save_steps=500,
+        save_total_limit=2,
+        load_best_model_at_end=True,
+        metric_for_best_model='eval_loss',
+        greater_is_better=False,
+        gradient_checkpointing=False,
         gradient_accumulation_steps=1,
         per_device_train_batch_size=BATCH_SIZE,
         per_device_eval_batch_size=BATCH_SIZE,
         num_train_epochs=10,
-        report_to='tensorboard'
+        report_to='wandb'
     )
