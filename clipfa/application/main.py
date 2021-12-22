9c5e75e4b6da98d7364ce10cf0c90748e755626e
Sajjad Ayoubi <43270899+sajjjadayobi@users.noreply.github.com>
2021-12-22 13:01:54 +0330

    Update main.py

--- clipfa/application/main.py
+++ clipfa/application/main.py
@@ -28,9 +28,12 @@ class CLIPDemo:
 
             Example:
             >>> demo = CLIPDemo(vision_encoder, text_encoder, tokenizer)
-            >>> demo.image_search('یک مرد و یک زن', image_paths)
-            >>> demo.zero_shot('./workers.jpg', class_list=['چند مرد','موز' ,'بیل'])
-            >>> demo.anology('./sunset.jpg', image_paths, additional_text='دریا')
+            >>> demo.compute_text_embeddings(['گاو' ,'اسب' ,'ماهی'])
+            >>> demo.compute_image_embeddings(image_paths)
+            >>> # test
+            >>> demo.image_search('یک مرد و یک زن')
+            >>> demo.zero_shot('./workers.jpg')
+            >>> demo.anology('./sunset.jpg', additional_text='دریا')
         """
         self.vision_encoder = vision_encoder.eval().to(device)
         self.text_encoder = text_encoder.eval().to(device)
@@ -38,9 +41,16 @@ class CLIPDemo:
         self.device = device
         self.tokenizer = tokenizer
         self.max_len = max_len
-        self.pre_compute = False
+        self.text_embeddings_ = None
+        self.image_embeddings_ = None
+        
 
-    def get_image_embeddings(self, image_paths: list):
+    def compute_image_embeddings(self, image_paths: list):
+        """ Compute image embeddings for a list of image paths
+            Args:
+                image_paths (list[str]): An image database
+        """
+        self.image_paths = image_paths
         datalodear = DataLoader(VisionDataset(
             image_paths=image_paths), batch_size=self.batch_size)
         embeddings = []
@@ -49,9 +59,14 @@ class CLIPDemo:
                 image_embedding = self.vision_encoder(
                     pixel_values=images.to(self.device)).pooler_output
                 embeddings.append(image_embedding)
-        return torch.cat(embeddings)
+        self.image_embeddings_ =  torch.cat(embeddings)
 
-    def get_text_embeddings(self, text: list):
+    def compute_text_embeddings(self, text: list):
+        """ Compute text embeddings for a list of sentences
+            Args:
+                text (list[str]): List of candidate classes (description of each class)
+        """
+        self.text = text
         dataloader = DataLoader(TextDataset(text=text, tokenizer=self.tokenizer, max_len=self.max_len),
                                 batch_size=self.batch_size, collate_fn=default_data_collator)
         embeddings = []
@@ -60,7 +75,7 @@ class CLIPDemo:
                 image_embedding = self.text_encoder(input_ids=tokens["input_ids"].to(self.device),
                                                     attention_mask=tokens["attention_mask"].to(self.device)).pooler_output
                 embeddings.append(image_embedding)
-        return torch.cat(embeddings)
+        self.text_embeddings_ = torch.cat(embeddings)
 
     def text_query_embedding(self, query: str = 'موز'):
         tokens = self.tokenizer(query, return_tensors='pt')
@@ -76,70 +91,56 @@ class CLIPDemo:
                 image.to(self.device)).pooler_output
         return image_embedding
 
-    def compute_embeddings(self, text: list, image_paths: list):
-        self.image_embeddings = self.get_image_embeddings(image_paths)
-        self.text_embeddings = self.get_text_embeddings(text)
-        self.pre_compute = True
-
     def most_similars(self, embeddings_1, embeddings_2):
         values, indices = torch.cosine_similarity(
             embeddings_1, embeddings_2).sort(descending=True)
-        return values.cpu().numpy(), indices.cpu().numpy()
+        return values.cpu(), indices.cpu()
 
-    def zero_shot(self, image_path: str, class_list: list):
+    def zero_shot(self, image_path: str):
         """ Zero shot image classification with label list 
             Args:
                 image_path (str): target image path that is going to be classified
-                class_list (list[str]): list of candidate classes 
         """
         image = Image.open(image_path)
-        class_embeddings = self.get_text_embeddings(class_list)
         image_embedding = self.image_query_embedding(image)
-        values, indices = self.most_similars(image_embedding, class_embeddings)
-        for i, sim in zip(indices, values):
-            print(
-                f'label: {class_list[i]} | similarity: {round(float(sim), 3)}')
+        values, indices = self.most_similars(image_embedding, self.text_embeddings_)
+        for i, sim in zip(indices, torch.softmax(values, dim=0)):
+            print(f'label: {self.text[i]}({round(float(sim), 3)})')
         plt.imshow(image)
         plt.axis('off')
 
-    def image_search(self, query: str, image_paths: list, top_k=10):
+    def image_search(self, query: str, top_k=10):
         """ Search images based on text query
             Args:
                 query (str): text query 
-                image_paths (list[str]): a bunch of image paths
                 top_k (int): number of relevant images 
         """
-        image_embeddings = self.get_image_embeddings(image_paths)
         query_embedding = self.text_query_embedding(query=query)
-        _, indices = self.most_similars(image_embeddings, query_embedding)
+        _, indices = self.most_similars(self.image_embeddings_, query_embedding)
 
-        matches = np.array(image_paths)[indices][:top_k]
+        matches = np.array(self.image_paths)[indices][:top_k]
         _, axes = plt.subplots(2, int(top_k/2), figsize=(15, 5))
         for match, ax in zip(matches, axes.flatten()):
             ax.imshow(Image.open(match).resize((224, 224)))
             ax.axis("off")
         plt.show()
 
-    def analogy(self, input_image_path: str, image_paths: list,
-                additional_text: str = 'برف', input_include=True):
+    def analogy(self, input_image_path: str, additional_text: str = 'برف', input_include=True):
         """ Analogies with embedding space arithmetic.
             Args:
                 input_image_path (str): The path to original image 
-                image_paths (list[str]): A database of images
+                additional_text (str): A word or sentence to add to input image
         """
         base_image = Image.open(input_image_path)
         image_embedding = self.image_query_embedding(base_image)
         additional_embedding = self.text_query_embedding(query=additional_text)
         new_image_embedding = image_embedding + additional_embedding
-        dataset_embeddings = self.get_image_embeddings(image_paths)
         _, indices = self.most_similars(
-            dataset_embeddings, new_image_embedding)
+            self.image_embeddings_, new_image_embedding)
 
-        new_image = Image.open(image_paths[indices[1 if input_include else 0]])
+        new_image = Image.open(self.image_paths[indices[1 if input_include else 0]])
         _, ax = plt.subplots(1, 2, dpi=100)
         ax[0].imshow(base_image.resize((250, 250)))
-        ax[0].set_title('original image')
         ax[0].axis('off')
         ax[1].imshow(new_image.resize((250, 250)))
-        ax[1].set_title('new image')
         ax[1].axis('off')
